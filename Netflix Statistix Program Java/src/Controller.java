56b8e66c602924ff990b5ea24a3d7f83a1dd4b79
Robin <robin.becx@hotmail.com>
2020-01-15 15:10:51 +0100

    begin van xml en controller connectie

--- Netflix Statistix Program Java/src/Controller.java
+++ Netflix Statistix Program Java/src/Controller.java
@@ -1,3 +1,21 @@
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Controller {
-}
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public TableColumn<,String> nameColumn;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        accountID = new TableColumn<>("Name");
+        accountID.setCellValueFactory(new PropertyValueFactory<>("name"));
+    }
+
+
